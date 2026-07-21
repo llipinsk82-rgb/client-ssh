ddee92bd56680a00af89bee8cf6cb2b5682c3f60
llipinsk82-rgb <llipinsk82@gmail.com>
2026-07-21 23:32:49 +0100

    Bump app version to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "eu.blackserv.clientssh"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
     }
 
     buildTypes {
