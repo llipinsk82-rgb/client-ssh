7110e4ea6ade0f6e8144fd5a6614503d2790203d
llipinsk82-rgb <llipinsk82@gmail.com>
2026-07-23 07:49:36 +0100

    Bump 0.3.3 test build code for in-place update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "eu.blackserv.clientssh"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
+        versionCode = 16
         versionName = "0.3.3"
     }
 
