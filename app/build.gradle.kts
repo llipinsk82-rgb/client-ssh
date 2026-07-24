6b4455b01ec4c9f09c26025625c02c95fd45846a
llipinsk82-rgb <llipinsk82@gmail.com>
2026-07-24 07:50:06 +0100

    Bump Client SSH build to 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "eu.blackserv.clientssh"
         minSdk = 26
         targetSdk = 36
-        versionCode = 32
+        versionCode = 33
         versionName = "0.3.4"
     }
 
