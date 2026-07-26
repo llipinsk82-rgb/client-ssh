13d65d3aae35e548af61ff65c5d59227e0a91029
llipinsk82-rgb <llipinsk82@gmail.com>
2026-07-26 12:37:51 +0100

    Bump telemetry build to 0.3.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "eu.blackserv.clientssh"
         minSdk = 26
         targetSdk = 36
-        versionCode = 38
-        versionName = "0.3.5"
+        versionCode = 39
+        versionName = "0.3.6"
     }
 
     signingConfigs {
