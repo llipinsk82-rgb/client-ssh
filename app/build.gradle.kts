7e76bcc2fc4d7ae5d8fae486c41b421253a7a33d
llipinsk82-rgb <llipinsk82@gmail.com>
2026-07-26 20:17:22 +0100

    Bump host key verification test build to versionCode 41

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "eu.blackserv.clientssh"
         minSdk = 26
         targetSdk = 36
-        versionCode = 40
+        versionCode = 41
         versionName = "0.3.6"
     }
 
