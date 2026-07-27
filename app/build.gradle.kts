528d4df9c2cb141a80b946fd8ad8d0b7de084a6b
llipinsk82-rgb <llipinsk82@gmail.com>
2026-07-27 16:36:20 +0100

    Bump versionCode for profile clone test build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "eu.blackserv.clientssh"
         minSdk = 26
         targetSdk = 36
-        versionCode = 44
+        versionCode = 45
         versionName = "0.3.6"
     }
 
