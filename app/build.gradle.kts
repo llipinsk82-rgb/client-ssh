492e9bf082c0da35ce99ded2b256d0128b6bf168
llipinsk82-rgb <llipinsk82@gmail.com>
2026-07-27 14:38:03 +0100

    Bump compact monitor test build to versionCode 44

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "eu.blackserv.clientssh"
         minSdk = 26
         targetSdk = 36
-        versionCode = 43
+        versionCode = 44
         versionName = "0.3.6"
     }
 
