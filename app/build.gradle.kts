5155e6bc79c63e68d24169a95f766456a2e89096
llipinsk82-rgb <llipinsk82@gmail.com>
2026-07-28 00:53:02 +0100

    Bump test build to version code 46

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "eu.blackserv.clientssh"
         minSdk = 26
         targetSdk = 36
-        versionCode = 45
+        versionCode = 46
         versionName = "0.3.6"
     }
 
